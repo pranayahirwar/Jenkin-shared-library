1d03c994a8c873cf3dd8c0432d2258d2075dd7f5
Pranay Ahirwar <56798886+pranayahirwar@users.noreply.github.com>
2022-12-13 23:17:18 +0530

    Update dockerBuild.groovy

--- var/dockerBuild.groovy
+++ var/dockerBuild.groovy
@@ -3,8 +3,9 @@
 def call() {
     echo "Building docker image..."
     sh "docker build -t trymi0/tryout:jam-100.1 ."
-    withCredentials([usernamePassword(credentialsId: 'dockerHubCred', usernameVariable:'USER', passwordVariable:'PSD')]) {
-        sh "echo $PSD | docker login -u $USER --password-stdin "
-        sh "docker push trymi0/tryout:jam-100.1"
+    echo "Image is build, but no pushed."
+//     withCredentials([usernamePassword(credentialsId: 'dockerHubCred', usernameVariable:'USER', passwordVariable:'PSD')]) {
+//         sh "echo $PSD | docker login -u $USER --password-stdin "
+//         sh "docker push trymi0/tryout:jam-100.1"
     }
 }
