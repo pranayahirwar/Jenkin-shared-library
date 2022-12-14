c192b1739ef51a8bfb274322c3c3b3444170f5f0
Trymi0 <pranayahirwar@gmail.com>
2022-12-14 21:03:48 +0530

    Initial commit, without unnecessary files

---
+++ README.md
@@ -0,0 +1,2 @@
+# Jenkin-shared-library
+Created to use same external groovy script, in different mircoservice application 📦🔧
---
+++ vars/dockerBuild.groovy
@@ -0,0 +1,12 @@
+#!/usr/bin/env groovy
+
+import com.example.Docker
+
+def call(String imageName) {
+    return new Docker(this).dockerbuildImage(imageName)
+//     withCredentials([usernamePassword(credentialsId: 'dockerHubCred', usernameVariable:'USER', passwordVariable:'PSD')]) {
+//         sh "echo $PSD | docker login -u $USER --password-stdin "
+//         sh "docker push $imageName"
+//  }
+}
+// $imageName = trymi0/tryout:jam-100.1
